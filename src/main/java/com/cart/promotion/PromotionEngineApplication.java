921b3a05bf3a5462f29214c10bff595a6ce11573
ckollip <ckollip@lowes.com>
2020-08-29 21:08:37 +0530

    Init

---
+++ src/main/java/com/cart/promotion/PromotionEngineApplication.java
@@ -0,0 +1,13 @@
+package com.cart.promotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromotionEngineApplication.class, args);
+	}
+
+}
